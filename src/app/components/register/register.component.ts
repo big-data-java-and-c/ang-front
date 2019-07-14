7f95526c938c6ba29a01001146e094093d94401e
Kamil Żarski <209526@edu.p.lodz.pl>
2019-07-15 00:54:56 +0200

    added registry

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,5 +1,10 @@
-import {Component, OnInit} from '@angular/core';
-import {FormBuilder, FormControl, FormGroup} from '@angular/forms';
+import { UserService } from './../../services/user.service';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+
+import { User } from './../../models/user';
+
 
 @Component({
   selector: 'app-register',
@@ -7,23 +12,59 @@ import {FormBuilder, FormControl, FormGroup} from '@angular/forms';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-  registerForm: FormGroup;
-  roles = ['ADMIN', 'STUDENT', 'TEACHER'];
 
-  constructor(private fb: FormBuilder) {
+
+  isSubmited = false;
+  registerForm: FormGroup;
+  roles = ['STUDENT', 'TEACHER'];
+
+  constructor(private fb: FormBuilder,
+    private toastrS: ToastrService,
+    private userService: UserService,
+  ) {
   }
 
   ngOnInit() {
     this.registerForm = this.fb.group({
-      email: '',
-      password: '',
-      displayName: '',
-      role: ''
+      email: ['', Validators.required, Validators.pattern[('^[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}$')]],
+      password: ['', Validators.required, Validators.minLength(8)],
+      confirmPassword: ['', Validators.required, Validators.minLength(8)],
+      displayName: ['', Validators.maxLength(20)],
+      role: 'STUDENT'
     });
-
   }
 
   onSubmit() {
 
+    this.isSubmited = true;
+    if (this.registerForm.invalid) {
+      console.log('invalid form registry');
+      return;
+    }
+    if (this.registerForm.value.password !== this.registerForm.value.confirmPassword) {
+
+      console.log(this.registerForm.value.password);
+      console.log(this.registerForm.value.confirmPassword);
+      console.log('password dont match');
+      return;
+    }
+
+    const temporarryUser: User = new User(this.registerForm.value.email,
+      this.registerForm.value.password,
+      this.registerForm.value.displayName,
+      this.registerForm.value.role,
+    );
+    console.log(temporarryUser);
+    this.userService.register(temporarryUser)
+      .toPromise()
+      .then(sukces => {
+        console.log(sukces);
+        console.log('succesfully registratio n');
+      })
+      .catch(e => {
+        console.log(e);
+        console.log('err already in use');
+      });
   }
 }
+
---
+++ src/app/services/user.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+
+import { HttpClient,HttpHeaders } from '@angular/common/http';
+import { User } from './../models/user';
+import { environment } from './../../environments/environment';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor(private http: HttpClient) { }
+
+  register(user: User) {
+    return this.http.post(`${environment.backendUrl}/api/auth/signup`, user);
+  }
+}
