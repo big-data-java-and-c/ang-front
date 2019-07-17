aa0b65a11c722fee1e086a856a195be790f81d0b
EkoGroszek <jachimczak3125@gmail.com>
2019-07-17 19:05:21 +0200

    added missing files

---
+++ src/app/components/student-grades-of-subject/student-grades-of-subject.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-student-grades-of-subject',
+  templateUrl: './student-grades-of-subject.component.html',
+  styleUrls: ['./student-grades-of-subject.component.css']
+})
+export class StudentGradesOfSubjectComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
