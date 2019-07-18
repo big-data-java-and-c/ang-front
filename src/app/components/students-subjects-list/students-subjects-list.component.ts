4554999f45008de80b6c23ac983a37fb32a683f8
EkoGroszek <jachimczak3125@gmail.com>
2019-07-18 18:12:47 +0200

    subject list almost works v2

--- src/app/components/students-subjects-list/students-subjects-list.component.ts
+++ src/app/components/students-subjects-list/students-subjects-list.component.ts
@@ -3,6 +3,9 @@ import {Subject} from '../../models/subject';
 import {SubjectServiceService} from '../../services/subject-service.service';
 import {animate, state, style, transition, trigger} from '@angular/animations';
 import {tap} from 'rxjs/operators';
+import {GradesService} from '../../services/grades.service';
+import {Grade} from '../../models/Grade';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-students-subjects-list',
@@ -19,34 +22,49 @@ import {tap} from 'rxjs/operators';
 export class StudentsSubjectsListComponent implements OnInit {
   subjects: Subject[] = [];
   dataSource: any;
-  // columnsToDisplay = ['name', 'weight', 'symbol', 'position'];
   columnsToDisplay = ['name'];
   expandedElement: PeriodicElement | null;
+  allStdentsGrades: Grade[];
+  studentId: string;
   private PeriodicElement: PeriodicElement = {
     name: '',
-    description: []
+    grades: [],
+    id: ''
   };
 
 
-  constructor(private subjectService: SubjectServiceService) {
+  constructor(
+    private subjectService: SubjectServiceService,
+    private gradeService: GradesService,
+    private route: ActivatedRoute,
+  ) {
   }
 
   ngOnInit() {
-    this.subjectService.getSubjects()
-      .subscribe(data => {
-          this.dataSource = ELEMENT_DATA;
-          this.subjects = data;
-          console.log(this.subjects.length);
-          this.addSubjectsToView();
-        }
-      )
-    ;
+    this.studentId = this.route.snapshot.paramMap.get('studentId');
 
+    this.gradeService.getAllStudentGrades(this.studentId).subscribe(data => {
+      this.allStdentsGrades = data;
+      this.subjectService.getSubjects()
+        .subscribe(innerData => {
+            this.dataSource = ELEMENT_DATA;
+            this.dataSource = []; // to musi być żeby sie za każdym wejściem w liste przedmiotów nie dodawały nowe
+            this.subjects = innerData;
+            this.addSubjectsToView();
+            console.log(this.allStdentsGrades[1].value_grade);
+          }
+        );
+    });
   }
 
   addSubjectsToView() {
     for (let i = 0; i < this.subjects.length; i++) {
-      this.dataSource.push({name: this.subjects[i][1], description: ['Tu będa oceny #ihope', ' coś innego']});
+      this.dataSource.push({
+        name: this.subjects[i][1],
+        grades: ['Tu będa oceny #ihope', ' coś innego'],
+        // grades: ['Tu będa oceny #ihope', ' coś innego'],
+        id: this.subjects[i][0]
+      });
       console.log(this.dataSource);
     }
   }
@@ -56,56 +74,57 @@ export class StudentsSubjectsListComponent implements OnInit {
 
 export interface PeriodicElement {
   name: string;
-  description: string[];
+  grades: string[];
+  id: string;
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [];
 //   {
 //     name: 'Hydrogen',
-//     description: `Hydrogen is a chemical element with symbol H and atomic number 1. With a standard
+//     grades: `Hydrogen is a chemical element with symbol H and atomic number 1. With a standard
 //         atomic weight of 1.008, hydrogen is the lightest element on the periodic table.`
 //   }, {
 //     name: 'Helium',
-//     description: `Helium is a chemical element with symbol He and atomic number 2. It is a
+//     grades: `Helium is a chemical element with symbol He and atomic number 2. It is a
 //         colorless, odorless, tasteless, non-toxic, inert, monatomic gas, the first in the noble gas
 //         group in the periodic table. Its boiling point is the lowest among all the elements.`
 //   }, {
 //     name: 'Lithium',
-//     description: `Lithium is a chemical element with symbol Li and atomic number 3. It is a soft,
+//     grades: `Lithium is a chemical element with symbol Li and atomic number 3. It is a soft,
 //         silvery-white alkali metal. Under standard conditions, it is the lightest metal and the
 //         lightest solid element.`
 //   }, {
 //     name: 'Beryllium',
-//     description: `Beryllium is a chemical element with symbol Be and atomic number 4. It is a
+//     grades: `Beryllium is a chemical element with symbol Be and atomic number 4. It is a
 //         relatively rare element in the universe, usually occurring as a product of the spallation of
 //         larger atomic nuclei that have collided with cosmic rays.`
 //   }, {
 //     name: 'Boron',
-//     description: `Boron is a chemical element with symbol B and atomic number 5. Produced entirely
+//     grades: `Boron is a chemical element with symbol B and atomic number 5. Produced entirely
 //         by cosmic ray spallation and supernovae and not by stellar nucleosynthesis, it is a
 //         low-abundance element in the Solar system and in the Earth's crust.`
 //   }, {
 //     name: 'Carbon',
-//     description: `Carbon is a chemical element with symbol C and atomic number 6. It is nonmetallic
+//     grades: `Carbon is a chemical element with symbol C and atomic number 6. It is nonmetallic
 //         and tetravalent—making four electrons available to form covalent chemical bonds. It belongs
 //         to group 14 of the periodic table.`
 //   }, {
 //     name: 'Nitrogen',
-//     description: `Nitrogen is a chemical element with symbol N and atomic number 7. It was first
+//     grades: `Nitrogen is a chemical element with symbol N and atomic number 7. It was first
 //         discovered and isolated by Scottish physician Daniel Rutherford in 1772.`
 //   }, {
 //     name: 'Oxygen',
-//     description: `Oxygen is a chemical element with symbol O and atomic number 8. It is a member of
+//     grades: `Oxygen is a chemical element with symbol O and atomic number 8. It is a member of
 //          the chalcogen group on the periodic table, a highly reactive nonmetal, and an oxidizing
 //          agent that readily forms oxides with most elements as well as with other compounds.`
 //   }, {
 //     name: 'Fluorine',
-//     description: `Fluorine is a chemical element with symbol F and atomic number 9. It is the
+//     grades: `Fluorine is a chemical element with symbol F and atomic number 9. It is the
 //         lightest halogen and exists as a highly toxic pale yellow diatomic gas at standard
 //         conditions.`
 //   }, {
 //     name: 'Neon',
-//     description: `Neon is a chemical element with symbol Ne and atomic number 10. It is a noble gas.
+//     grades: `Neon is a chemical element with symbol Ne and atomic number 10. It is a noble gas.
 //         Neon is a colorless, odorless, inert monatomic gas under standard conditions, with about
 //         two-thirds the density of air.`
 //   },
--- src/app/services/grades.service.ts
+++ src/app/services/grades.service.ts
@@ -3,6 +3,7 @@ import {Injectable} from '@angular/core';
 import {Observable} from 'rxjs';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {environment} from './../../environments/environment';
+import {Grade} from '../models/Grade';
 
 
 @Injectable({
@@ -19,6 +20,9 @@ export class GradesService {
     console.log('userId -> ');
     console.log(sessionStorage.getItem('userId'));
     return this.http.get<number[]>(`${environment.backendUrl}/api/grade/grades/${id}/student/${sessionStorage.getItem('userId')}`);
+  }
+  getAllStudentGrades(studentId: string){
+    return this.http.get<Grade[]>(`${environment.backendUrl}/api/grade/all/${studentId}`);
   }
 
   getgetGradesValueBySubjaectIdAndStudentId(subjectId: string, studentId: string) {
